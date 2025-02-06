a1e0d4057b862dcff5946157f9c62ef332f66845
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-02-06 13:11:41 +0100

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
     return (
         <div>
             <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+
+                <div className="">
+                    <img src={assets.logo} className="mb-5 w-32" alt='' />
+                    <p className='w-full md:w-2/3 text-gray-600'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ab animi laborum odit ducimus eaque praesentium quos accusantium veritatis, error saepe quia corporis temporibus nostrum impedit, reprehenderit debitis officiis distinctio!
+                    </p>
+                </div>
+
                 <div className="flex flex-col gap-4">
                     <p className='font-semibold'>ABOUT US</p>
                     <p className='text-gray-400'>About Us</p>
