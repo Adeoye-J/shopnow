9518887ed08e0c95604237566f11471081f68b76
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-02-11 12:38:09 +0100

    home and collections update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,7 +13,25 @@ const Footer = () => {
                     </p>
                 </div>
 
-                <div className="flex flex-col gap-4">
+                <div className="">
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-2 text-gray-400'>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+
+                <div className="">
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-2 text-gray-400'>
+                        <li>+234-815-0420-521</li>
+                        <li>contact@shopnow.com</li>
+                    </ul>
+                </div>
+
+                {/* <div className="flex flex-col gap-4">
                     <p className='font-semibold'>ABOUT US</p>
                     <p className='text-gray-400'>About Us</p>
                     <p className='text-gray-400'>Contact Us</p>
@@ -30,7 +48,12 @@ const Footer = () => {
                     <p className='text-gray-400'>Return Policy</p>
                     <p className='text-gray-400'>Terms Of Use</p>
                     <p className='text-gray-400'>Security</p>
-                </div>
+                </div> */}
+            </div>
+
+            <div className="">
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright 2024@ shopnow.com - All Right Reserved.</p>
             </div>
         </div>
     )
